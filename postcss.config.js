45f864f86b09e4f0dc52a327ee2eaa86769ca58d
mitmarcus <114725463+mitmarcus@users.noreply.github.com>
2024-09-21 18:18:33 +0200

    lighthouse: revert changes as it doesn't improve performance

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,8 @@
 module.exports = {
   plugins: [
     'postcss-import',
-
-    // Tailwind CSS plugins:
     'tailwindcss/nesting',
     'tailwindcss',
     'autoprefixer',
-
-    // PurgeCSS plugin only in production:
-    [
-      '@fullhuman/postcss-purgecss',
-      process.env.NODE_ENV === 'production'
-        ? {
-            content: ['./**/*.html'],
-          }
-        : false,
-    ],
   ],
 }
